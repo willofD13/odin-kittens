535f2130a4c722f165d5dcd4acbdb4cb50dcfec6
willofD13 <leonidastsiakas13@gmail.com>
2023-12-11 19:42:22 +0200

    Create controller actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,4 +2,36 @@ class KittensController < ApplicationController
     def index 
         @kittens = Kitten.all 
     end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def show
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def edit
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+
+        if @kitten.save
+            redirect_to :index
+        else
+            redirect_to :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @kitten = Kitten.find(params[:id])
+
+        if @kitten.update(kitten_params)
+            redirect_to @kitten
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
 end
