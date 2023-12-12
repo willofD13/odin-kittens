aa79bfa235a0f701b060d81f5cd29d3bb340e626
willofD13 <leonidastsiakas13@gmail.com>
2023-12-12 18:05:51 +0200

    Add validations to kitten model

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,5 @@
 class Kitten < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :cuteness, presence: true, numericality: { less_than_or_equal_to: 10 }
+    validates :softness, presence: true, numericality: { less_than_or_equal_to: 10 }
 end
